b171d575400a1528716311cd7ce603d70e055269
Gaston Khouri <gastonlozano2016@yahoo.com>
2023-12-20 06:09:26 -0400

    test

---
+++ src/routes/layout.tsx
@@ -0,0 +1,13 @@
+import { component$, Slot, useStyles$ } from "@builder.io/qwik";
+
+
+import styles from "./styles.css?inline";
+
+
+export default component$(() => {
+  useStyles$(styles);
+
+  return (
+    <Slot/>
+  );
+});
